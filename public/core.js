ba24100c3d2f5b20ccdadac9baa25112adad9192
Chrissie <chrissie.deist@gmail.com>
2016-05-30 15:53:08 -0500

    Add ability to delete todos

--- public/core.js
+++ public/core.js
@@ -12,11 +12,22 @@ function mainController($scope, $http) {
             console.log('Error: ' + data);
         });
 
-    // when submitting the add form, send the text to the node API
     $scope.createTodo = function() {
         $http.post('/api/todos', $scope.formData)
             .success(function(data) {
-                $scope.formData = {}; // clear the form so our user is ready to enter another
+                $scope.formData = {};
+                $scope.todos = data;
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+    };
+
+    $scope.deleteTodo = function(id) {
+        console.log("delete it: " + id);
+        $http.delete('/api/todos/' + id)
+            .success(function(data) {
                 $scope.todos = data;
                 console.log(data);
             })
--- routes/todos.js
+++ routes/todos.js
@@ -32,21 +32,18 @@ module.exports = function(app) {
     });
   });
 
-
-  // delete a todo
   app.delete('/api/todos/:todo_id', function(req, res) {
-    // Todo.remove({
-    //   _id : req.params.todo_id
-    // }, function(err, todo) {
-    //   if (err)
-    //   res.send(err);
-    //
-    // // get and return all the todos after you create another
-    // Todo.find(function(err, todos) {
-    //   if (err)
-    //   res.send(err)
-    //   res.json(todos);
-    // });
-    // });
+    Todo.remove({
+      _id : req.params.todo_id
+    }, function(err, todo) {
+      if (err)
+        res.send(err);
+
+      Todo.find(function(err, todos) {
+        if (err)
+          res.send(err)
+        res.json(todos);
+      });
+    });
   });
 }
